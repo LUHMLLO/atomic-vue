080bcbf9617d6f7424195098b2eee5297e54e366
LUHMLLO <luhmllo06@protonmail.com>
2024-02-28 10:23:33 -0400

    fixed some imports

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,34 +1,31 @@
 import axios from 'axios';
 import { atom } from 'nanostores';
-import { $token } from '@api/token';
-import { Profile } from '@/commons/interfaces/profiles';
-import { HandleApiError } from '@/commons/utils/handleApiErrors';
+import { $token } from '@api/token.ts';
+import { Profile } from '@interfaces/profiles.ts';
+import { HandleApiError } from '@utils/handleApiErrors.ts';
 
 export const $profile = atom<Profile | null>(null);
 
 export function $setProfile(profile: Profile | null) {
-    $profile.set(profile);
+	$profile.set(profile);
 }
 
 export async function GetProfile() {
-    const jwt = $token.get();
+	const jwt = $token.get();
 
-    try {
-        const response = await axios.get(
-            import.meta.env.VITE_ENDPOINT_PROFILE,
-            {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
+	try {
+		const response = await axios.get(import.meta.env.VITE_ENDPOINT_PROFILE, {
+			headers: {
+				Authorization: `Bearer ${jwt}`,
+				'Content-Type': 'application/json',
+			},
+		});
 
-        if (response.status === 200) {
-            const profile: Profile = await response.data.payload;
-            $setProfile(profile);
-        }
-    } catch (error: unknown) {
-        HandleApiError(error)
-    }
+		if (response.status === 200) {
+			const profile: Profile = await response.data.payload;
+			$setProfile(profile);
+		}
+	} catch (error: unknown) {
+		HandleApiError(error);
+	}
 }
--- src/api/token.ts
+++ src/api/token.ts
@@ -1,40 +1,47 @@
 import axios from 'axios';
 import { persistentAtom } from '@nanostores/persistent';
-import { GetProfile } from '@/api/profile';
-import { $setNotification } from '@/commons/stores/notifications';
-import { Token } from '@/commons/interfaces/tokens';
-import { LoginDTO } from '@/commons/interfaces/dtos';
-import { HandleApiError } from '@/commons/utils/handleApiErrors';
+import { GetProfile } from '@api/profile.ts';
+import { $setNotification } from '@stores/notifications.ts';
+import { Token } from '@interfaces/tokens.ts';
+import { LoginDTO } from '@interfaces/dtos.ts';
+import { HandleApiError } from '@utils/handleApiErrors.ts';
 
 export const $token = persistentAtom<Token | null>('token', null, {
-    encode: JSON.stringify,
-    decode: JSON.parse,
+	encode: JSON.stringify,
+	decode: JSON.parse,
 });
 
 export function $setToken(token: Token | null) {
-    $token.set(token);
+	$token.set(token);
 }
 
-export async function GetToken(data: LoginDTO, navigate: (path: string) => void) {
-    try {
-        const response = await axios.post(
-            import.meta.env.VITE_ENDPOINT_LOGIN,
-            data,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
+export async function GetToken(
+	data: LoginDTO,
+	navigate: (path: string) => void
+) {
+	try {
+		const response = await axios.post(
+			import.meta.env.VITE_ENDPOINT_LOGIN,
+			data,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
 
-        if (response.status === 201) {
-            const token: Token = await response.data.payload.token;
-            $setToken(token);
-            GetProfile();
-            $setNotification({ title: `Bienvenido de vuelta ${data.username}`, content: '', icon: 'check_circle' })
-            navigate('/dashboard');
-        }
-    } catch (error: unknown) {
-        HandleApiError(error)
-    }
+		if (response.status === 201) {
+			const token: Token = await response.data.payload.token;
+			$setToken(token);
+			GetProfile();
+			$setNotification({
+				title: `Bienvenido de vuelta ${data.username}`,
+				content: '',
+				icon: 'check_circle',
+			});
+			navigate('/dashboard');
+		}
+	} catch (error: unknown) {
+		HandleApiError(error);
+	}
 }
