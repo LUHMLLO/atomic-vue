d0ac6950cf8dad00fe44c3910efcb566aa14090e
LUHMLLO <luhmllo06@protonmail.com>
2024-02-28 10:31:40 -0400

    updated dependencies and fixed import aliases

--- src/router.ts
+++ src/router.ts
@@ -1,102 +1,104 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Layout_Auth from '@layouts/auth.vue'
-import Layout_App from '@layouts/app.vue'
-import Layout_Lab from "@layouts/lab.vue"
-import Layout_Error from '@layouts/error.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Layout_Auth from '@layouts/auth.vue';
+import Layout_App from '@layouts/app.vue';
+import Layout_Lab from '@layouts/lab.vue';
+import Layout_Error from '@layouts/error.vue';
 
 const routes = [
-    {
-        path: '/auth/login',
-        component: () => import('@routes/auth/login.vue'),
-        meta: {
-            title: 'Gizmo - Login',
-            layout: Layout_Auth,
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/auth/recovery',
-        component: () => import('@routes/auth/recovery.vue'),
-        meta: {
-            title: 'Gizmo - Recovery',
-            layout: Layout_Auth,
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/auth/signup',
-        component: () => import('@routes/auth/signup.vue'),
-        meta: {
-            title: 'Gizmo - Signup',
-            layout: Layout_Auth,
-            requiresAuth: false
-        }
-    },
-    {
-        path: '/app/home',
-        alias: '/',
-        component: () => import('@routes/app/dashboard.vue'),
-        meta: {
-            title: 'Gizmo - Home',
-            layout: Layout_App,
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/lab/atoms',
-        component: () => import('@routes/inspect/atoms.vue'),
-        meta: {
-            title: 'Components - Atoms',
-            layout: Layout_Lab,
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/lab/molecules',
-        component: () => import('@routes/inspect/molecules.vue'),
-        meta: {
-            title: 'Components - Molecules',
-            layout: Layout_Lab,
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/lab/organisms',
-        component: () => import('@routes/inspect/organisms.vue'),
-        meta: {
-            title: 'Components - Organisms',
-            layout: Layout_Lab,
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/lab/widgets',
-        component: () => import('@routes/inspect/widgets.vue'),
-        meta: {
-            title: 'Components - widgets',
-            layout: Layout_Lab,
-            requiresAuth: true
-        }
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        component: () => import('@routes/404.vue'),
-        meta: {
-            title: 'Gizmo - 404',
-            layout: Layout_Error,
-            requiresAuth: false
-        }
-    },
-]
+	{
+		path: '/auth/login',
+		component: () => import('@routes/auth/login.vue'),
+		meta: {
+			title: 'Gizmo - Login',
+			layout: Layout_Auth,
+			requiresAuth: false,
+		},
+	},
+	{
+		path: '/auth/recovery',
+		component: () => import('@routes/auth/recovery.vue'),
+		meta: {
+			title: 'Gizmo - Recovery',
+			layout: Layout_Auth,
+			requiresAuth: false,
+		},
+	},
+	{
+		path: '/auth/signup',
+		component: () => import('@routes/auth/signup.vue'),
+		meta: {
+			title: 'Gizmo - Signup',
+			layout: Layout_Auth,
+			requiresAuth: false,
+		},
+	},
+	{
+		path: '/app/home',
+		alias: '/',
+		component: () => import('@routes/app/dashboard.vue'),
+		meta: {
+			title: 'Gizmo - Home',
+			layout: Layout_App,
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/lab/atoms',
+		component: () => import('@routes/inspect/atoms.vue'),
+		meta: {
+			title: 'Components - Atoms',
+			layout: Layout_Lab,
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/lab/molecules',
+		component: () => import('@routes/inspect/molecules.vue'),
+		meta: {
+			title: 'Components - Molecules',
+			layout: Layout_Lab,
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/lab/organisms',
+		component: () => import('@routes/inspect/organisms.vue'),
+		meta: {
+			title: 'Components - Organisms',
+			layout: Layout_Lab,
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/lab/widgets',
+		component: () => import('@routes/inspect/widgets.vue'),
+		meta: {
+			title: 'Components - widgets',
+			layout: Layout_Lab,
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		component: () => import('@routes/404.vue'),
+		meta: {
+			title: 'Gizmo - 404',
+			layout: Layout_Error,
+			requiresAuth: false,
+		},
+	},
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+	history: createWebHistory(),
+	routes,
+});
 
 router.beforeEach((to, _from, next) => {
-    document.title = to.meta.title ? String(to.meta.title).trim() : "Hytech Gizmo";
-    next()
-})
+	document.title = to.meta.title
+		? String(to.meta.title).trim()
+		: 'Hytech Gizmo';
+	next();
+});
 
-export default router
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -7,24 +8,24 @@ const port = parseInt(env.VITE_PORT) || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port,
-  },
-  preview: {
-    port,
-  },
-  resolve: {
-    alias: {
-      '@': './src/',
-      '@api': './src/api/',
-      '@interfaces': './src/commons/interfaces/',
-      '@stores': './src/commons/stores/',
-      '@styles': './src/commons/styles/',
-      '@utils': './src/commons/utils/',
-      '@components': './src/components/',
-      '@layouts': './src/layouts/',
-      '@routes': './src/routes/',
-    },
-  },
+	plugins: [vue()],
+	server: {
+		port,
+	},
+	preview: {
+		port,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src/'),
+			'@api': path.resolve(__dirname, './src/api/'),
+			'@interfaces': path.resolve(__dirname, './src/commons/interfaces/'),
+			'@stores': path.resolve(__dirname, './src/commons/stores/'),
+			'@styles': path.resolve(__dirname, './src/commons/styles/'),
+			'@utils': path.resolve(__dirname, './src/commons/utils/'),
+			'@components': path.resolve(__dirname, './src/components/'),
+			'@layouts': path.resolve(__dirname, './src/layouts/'),
+			'@routes': path.resolve(__dirname, './src/routes/'),
+		},
+	},
 });
